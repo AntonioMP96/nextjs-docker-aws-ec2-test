b7ba52386b3d3852ef22a2c3af690bdc844451cd
Tony <joseantonio.martinez@cykadas.com>
2024-05-03 13:28:46 -0600

    - Creando API de conexion con DB

---
+++ app/api/auth/signup/route.ts
@@ -0,0 +1,51 @@
+import { NextResponse } from "next/server"
+import User from '@/models/user'
+import bcrypt from "bcryptjs"
+import {connectDB} from '@/lib/mongodb'
+
+
+
+export async function POST(request: Request) {
+
+    const {username, email, password} = await request.json()
+    console.log(username, email, password)
+
+    if (!password || password.length < 6 ) return NextResponse.json({
+        message: "La contraseña debe tener al menos 6 caracteres!"
+    }, {
+        status: 400
+    })
+
+    try {
+        await connectDB()
+
+        // -- checando existencia del usuario --
+        const userFound = await User.findOne({email})
+        if (userFound) return NextResponse.json({
+            message: "El email ya existe!"
+        }, {
+            status: 409
+        })
+
+        // -- guardando nuevo usuario --
+        const hashedPassword = await bcrypt.hash(password, 12)
+        const user = new User({
+            email,
+            username,
+            password: hashedPassword
+        })
+        const savedUser = await user.save()
+        console.log('USUARIO GUARDADO:', savedUser)
+        return NextResponse.json(savedUser)
+
+    } catch (error) {
+        console.log('ERROR:', error)
+        if (error instanceof Error) {
+            return NextResponse.json({
+                message: error.message,
+            }, {
+                status: 400,
+            })
+        }
+    }
+}
