99c50b96a7a9aa718606a1f45bb6dec2280ff11d
Tony <joseantonio.martinez@cykadas.com>
2024-05-03 13:28:25 -0600

    - Creando modelo de usuarios (V1)

---
+++ models/user.ts
@@ -0,0 +1,28 @@
+import { Schema, model, models } from 'mongoose'
+
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: [true, 'El email es requerido!'],
+        match: [
+            /^\w+([\-]?\w+)*@\w+([\-]?\w+)*(\.\w{2,3})+$/,
+            "El email no es valido"
+        ]
+    },
+    password: {
+        type: String,
+        required: [true, "La contraseña es requerida!"],
+        select: false
+    },
+    username: {
+        type: String,
+        required: [true, 'El nombre es requerido!'],
+        minLength: [4, "El nombre debe tener al menos 4 caracteres!"],
+        kMaxLength: [50, "El nombre debe tener menos de 50 caracteres!"]
+    },
+})
+
+const User = models.User || model('User', userSchema)
+export default User
