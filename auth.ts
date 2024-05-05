6b97494cd0d110d484612c674b08f3c612c00b32
Tony Laptop <jmlegoman2@gmail.com>
2024-05-04 19:28:47 -0600

    - iniciando auth

--- auth.ts
+++ auth.ts
@@ -1,7 +1,37 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
+import Credentials from "next-auth/providers/credentials"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [GitHub, Google],
+    providers: [
+        GitHub, 
+        Google,
+        // Credentials({
+        //     // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+        //     // e.g. domain, username, password, 2FA token, etc.
+        //     credentials: {
+        //       email: {},
+        //       password: {},
+        //     },
+        //     authorize: async (credentials) => {
+        //       let user = null
+       
+        //       // logic to salt and hash password
+        //       const pwHash = saltAndHashPassword(credentials.password)
+       
+        //       // logic to verify if user exists
+        //       user = await getUserFromDb(credentials.email, pwHash)
+       
+        //       if (!user) {
+        //         // No user found, so this is their first attempt to login
+        //         // meaning this is also the place you could do registration
+        //         throw new Error("User not found.")
+        //       }
+       
+        //       // return user object with the their profile data
+        //       return user
+        //     },
+        // }),
+    ],
 })
