3e32180d1ae376b91790a0da4e25bdc89c551791
Tony Laptop <jmlegoman2@gmail.com>
2024-05-05 22:18:30 -0600

    - Agregando loading state

--- app/components/RegisterForm.tsx
+++ app/components/RegisterForm.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { signIn } from "@/auth"
 import axios, {AxiosError} from 'axios'
 import { redirect } from "next/navigation"
 import { FormEvent, useState } from 'react'
@@ -9,8 +10,10 @@ import { IoEyeOutline, IoEyeOffOutline} from 'react-icons/io5'
 export const RegisterForm = () => {
     const [error, setError] = useState()
     const [showPass, setShowPass] = useState(false)
+    const [loading, setLoading] = useState(false)
 
     const handleSubmit = async (e:FormEvent<HTMLFormElement>) => {
+        setLoading(true)
         console.log('Entrando en handle submit')
         e.preventDefault()
 
@@ -23,6 +26,7 @@ export const RegisterForm = () => {
                 email: formData.get('email'),
             })
             console.log('RESPUESTA DEL SIGNUP:', signupResponse)
+            setLoading(false)
             // redirect('/')
 
             // "use server"
@@ -37,56 +41,79 @@ export const RegisterForm = () => {
             if (error instanceof AxiosError) {
                 setError(error.response?.data.message)
             }
+            setLoading(false)
         }
     }
 
     return (
+
         <form 
         action=""
         onSubmit={handleSubmit}
         className="flex flex-col space-y-5">
-        {error && <span className='bg-red-500 p-2 text-center text-white rounded-lg antialiased'>
-        {error}
-        </span>}
-        <input 
-        name='username'
-        type="text" 
-        title="Nombre de usuario"
-        placeholder="Usuario"
-        className="flex px-3 py-2 md:px-4 md:py-3 border-2 border-black rounded-lg font-medium placeholder:font-normal" 
-        />
-        <input 
-        name='email'
-        type="email" 
-        title="Correo electronico"
-        placeholder="Correo"
-        className="flex px-3 py-2 md:px-4 md:py-3 border-2 border-black rounded-lg font-medium placeholder:font-normal" 
-        />
-        <div className="relative w-full">
-        <input 
-        name='password'
-        type={showPass ? "text" : "password"} 
-        title="Contraseña"
-        placeholder="Contraseña"
-        className={`w-full flex px-3 py-2 md:px-4 md:py-3 border-2 border-black rounded-lg font-medium placeholder:font-normal`} 
-        />
+            {error && <span className='bg-red-500 p-2 text-center text-white rounded-lg antialiased'>
+            {error}
+            </span>}
+            <input 
+            disabled={loading}
+            name='username'
+            type="text" 
+            title="Nombre de usuario"
+            placeholder="Usuario"
+            className={`flex px-3 py-2 md:px-4 md:py-3 
+            border-2 border-black rounded-lg 
+            font-medium placeholder:font-normal
+            ${loading && 'animate-pulse'}
+            `} 
+            />
+            <input 
+            disabled={loading}
+            name='email'
+            type="email" 
+            title="Correo electronico"
+            placeholder="Correo"
+            className={`
+            flex px-3 py-2 md:px-4 md:py-3 
+            border-2 border-black rounded-lg 
+            font-medium placeholder:font-normal
+            ${loading && 'animate-pulse'}
+            `} 
+            />
+            <div className="relative w-full">
+                <input 
+                disabled={loading}
+                name='password'
+                type={showPass ? "text" : "password"} 
+                title="Contraseña"
+                placeholder="Contraseña"
+                className={`
+                w-full flex px-3 py-2 md:px-4 md:py-3 
+                border-2 border-black rounded-lg 
+                font-medium placeholder:font-normal
+                ${loading && 'animate-pulse'}
+                `} 
+                />
 
-        <button 
-        type="button"
-        title={showPass ? "Esconder" : "Mostrar"}
-        onClick={() => setShowPass(showPass ? false : true)}
-        className="absolute inset-y-0 right-0 pr-3 hover:text-slate-400 transition-all">
-        {showPass ? <IoEyeOutline className='text-2xl'/> : <IoEyeOffOutline className='text-2xl'/>}
-        </button>
-        </div>
+                <button 
+                type="button"
+                title={showPass ? "Esconder" : "Mostrar"}
+                onClick={() => setShowPass(showPass ? false : true)}
+                className={`absolute inset-y-0 right-0 pr-3 hover:text-slate-400 transition-all`}>
+                {showPass ? <IoEyeOutline className='text-2xl'/> : <IoEyeOffOutline className='text-2xl'/>}
+                </button>
+            </div>
 
-        <button className={`
-        flex items-center justify-center flex-none 
-        px-3 py-2 md:px-4 md:py-3 rounded-lg font-medium 
-        border-2  border-black bg-black text-white
-        `}>
-        Confirmar
-        </button>
+            <button 
+            disabled={loading}
+            className={`
+            flex items-center justify-center flex-none 
+            px-3 py-2 md:px-4 md:py-3 rounded-lg font-medium 
+            border-2  border-black bg-black text-white
+            disabled:bg-gray-400 disabled:text-slate-600 disabled:cursor-no-drop
+            ${loading && 'animate-pulse'}
+            `}>
+            Confirmar
+            </button>
         </form>
     )
 }
